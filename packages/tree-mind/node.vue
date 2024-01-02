46cb7f84a33443ecb0a25650f3b1bb609e33e2f0
liujianzeng <1623685118@qq.com>
2024-01-02 15:27:30 +0800

    建仓

---
+++ packages/tree-mind/node.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-show="expanded">{{ node.label }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Node',
+  props: {
+    node: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {
+      expanded: true
+    };
+  }
+};
+</script>
---
+++ packages/tree-mind/tree.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tree-mind">
+    <div v-for="node in data" :key="node.id">
+      <Node :node="node"></Node>
+    </div>
+    <div v-if="data.length===0">empty list</div>
+  </div>
+</template>
+
+<script>
+import Node from './node.vue';
+
+export default {
+  name: 'TreeMind',
+  components: {Node},
+  props: {
+    data: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
